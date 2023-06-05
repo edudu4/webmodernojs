ca0e050eafb6323ed686cc6e85de519666f47584
Eduardo Alves <eduardoa8142@gmail.com>
2023-06-05 18:53:48 -0300

    JavaScript Fundamentos

---
+++ numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7/0) //infinity
+console.log("10"/2) //resultado é 5
+console.log("Show!" * 2) // NaN - Not a Number
+console.log(0.1 + 0.7) // operação com especificação imprecisa (0.7999)
+//console.log(10.toString()) erro
+console.log((10).toString())
